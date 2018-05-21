284899f8b7e2af7e3e7c88b31694bddc482dbf0b
chenjiandongx <chenjiandongx@qq.com>
2018-05-21 09:10:14 +0800

    Format: 格式化代码

--- wallpaper.py
+++ wallpaper.py
@@ -1,19 +1,32 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+
 import os
 import re
 import threading
 import time
 from multiprocessing import Pool, cpu_count
+
 import requests
 
-dir_path = r"e:\wallpaper"      # 图片存放路径
-fmt = "http://sj.zol.com.cn{}"
-headers = {'X-Requested-With': 'XMLHttpRequest',
-           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                         'Chrome/56.0.2924.87 Safari/537.36'}
+# 图片存放路径
+SAVE_PATH = r"D:\wallpaper"
+CRAWL_URL = "http://sj.zol.com.cn{}"
+HEADERS = {
+    "X-Requested-With": "XMLHttpRequest",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 "
+    "(KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+}
+
 
 def mkdir(folder_name):
-    """ 新建文件夹并切换到该目录下 """
-    path = os.path.join(dir_path, folder_name)
+    """
+    新建文件夹并切换到该目录下
+
+    :param folder_name: 文件夹名称
+    """
+    path = os.path.join(SAVE_PATH, folder_name)
     # 如果目录已经存在就不用再次爬取了，去重，提高效率。存在返回 False，否则反之
     if not os.path.exists(path):
         os.makedirs(path)
@@ -23,76 +36,109 @@ def mkdir(folder_name):
     print("Folder has existed!")
     return False
 
-def save(src, name):
-    """ 保存图片到本地 """
+
+def save_images(src, name):
+    """
+    保存图片到本地
+
+    :param src: 图片 src
+    :param name: 保存图片名
+    """
     try:
-        img = requests.get(src, headers=headers)
-        with open(name + ".jpg", 'ab') as f:
+        img = requests.get(src, headers=HEADERS)
+        with open(name + ".jpg", "ab") as f:
             f.write(img.content)
-            print("Success!")
+            print("{}.jpg save Successfully".format(name))
     except:
-        print("Failed!")
+        pass
 
-def clear(dir):
-    """ 删除空文件夹 """
-    if os.path.exists(dir):
-        if os.path.isdir(dir):
-            for d in os.listdir(dir):
-                path = os.path.join(dir, d)
+
+def clear(dir_path):
+    """
+    删除空文件夹
+
+    :param dir_path: 文件夹路径
+    """
+    if os.path.exists(dir_path):
+        if os.path.isdir(dir_path):
+            for d in os.listdir(dir_path):
+                path = os.path.join(dir_path, d)
                 if os.path.isdir(path):
-                    clear(path)     # 递归删除空文件夹
-        if not os.listdir(dir):
-            os.rmdir(dir)
-            print("remove the empty dir: {}".format(dir))
+                    # 递归删除空文件夹
+                    clear(path)
+        if not os.listdir(dir_path):
+            os.rmdir(dir_path)
+            print("remove the empty dir: {}".format(dir_path))
 
-lock = threading.Lock()
 
-def geturls():
-    """ 获取壁纸套图地址 """
+def get_urls():
+    """
+    获取壁纸套图地址
+
     # 1080x1920(iphone6s plus)
     # 800x1280
     # 768x1280
     # 750x1334(iphone6)
     # 720x1280
     # 640x1336(iphone5s)
+    """
     url = "http://sj.zol.com.cn/bizhi/1080x1920/{}.html"
     _urls = set()
     for url in [url.format(page) for page in range(1, 51)]:
-        r = requests.get(url, headers=headers).text
-        for u in [fmt.format(u) for u in re.findall('<a class="pic" href="(.*?)"', r)]:
+        req = requests.get(url, headers=HEADERS).text
+        for u in [
+            CRAWL_URL.format(u)
+            for u in re.findall('<a class="pic" href="(.*?)"', req)
+        ]:
             _urls.add(u)
     return _urls
 
+
+lock = threading.Lock()
+
+
 def run(url):
-    """ 启动爬虫 """
+    """
+    启动爬虫
+    """
     try:
-        result = requests.get(url, headers=headers).text
+        html = requests.get(url, headers=HEADERS).text
         # 壁纸套图名，也作文件夹名
-        title = re.findall('<h1><a href=.*?>(.*?)</a.*?</h1>', result)[0]
+        title = re.findall("<h1><a href=.*?>(.*?)</a.*?</h1>", html)[0]
         # 壁纸套图张数
-        max_cnt = re.findall('</a><span>（(.*?)）</span>', result)[0].split("/")[1]
+        max_cnt = re.findall("</a><span>（(.*?)）</span>", html)[0].split("/")[1]
         with lock:
-            if mkdir(title):
-                # 按点击下一页循环下载
-                for _ in range(int(max_cnt)):
-                    next_page = re.findall('class ="next" href="(.*?)"', result)[0]
-                    img_url = re.findall('id="750x1334" href="(.*?)">750x1334</a>', result)
-                    # 像素未达到 750x1334 的不进行下载
-                    img = img_url[0] if img_url else []
-                    if img:
-                        r = requests.get(fmt.format(img), headers=headers).text
-                        img_src = re.findall('<img src="(.*?)"', r)[0]
-                        save(img_src, img[-12:-5])
-                    result = requests.get(fmt.format(next_page), headers=headers).text
+            if not mkdir(title):
+                return
+            # 按点击下一页循环下载
+            for _ in range(int(max_cnt)):
+                next_page = re.findall('class ="next" href="(.*?)"', html)
+                img_url = re.findall(
+                    'id="750x1334" href="(.*?)">750x1334</a>', html
+                )
+                # 像素未达到 750x1334 的不进行下载
+                img = img_url[0] if img_url else []
+                if img:
+                    req = requests.get(
+                        CRAWL_URL.format(img), headers=HEADERS
+                    ).text
+                    img_src = re.findall('<img src="(.*?)"', req)[0]
+                    save_images(img_src, img[-12:-5])
+                html = requests.get(
+                    CRAWL_URL.format(next_page[0]), headers=HEADERS
+                ).text
+
     except Exception as e:
         print(e)
 
+
 if __name__ == "__main__":
-    urls = geturls()
+    urls = get_urls()
     pool = Pool(processes=cpu_count())
     try:
         pool.map(run, urls)
     except:
         time.sleep(30)
         pool.map(run, urls)
-    # clear(dir_path)   # 由于有些套图图片像素大小未达到要求，文件夹为空需要清理
+    # 由于有些套图图片像素大小未达到要求，文件夹为空需要清理
+    clear(SAVE_PATH)
--- wallpaper_redis.py
+++ wallpaper_redis.py
@@ -1,24 +1,36 @@
+#!/usr/bin/env python
+# coding=utf-8
+
 import os
 import re
+
 import requests
 import redis
 
-rd = redis.Redis(host="localhost")      # 本机作为 master
-headers = {'X-Requested-With': 'XMLHttpRequest',
-           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 '
-                         '(KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
+rd = redis.Redis(host="localhost")  # 本机作为 master
+HEADERS = {
+    "X-Requested-With": "XMLHttpRequest",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 "
+    "(KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+}
+SAVE_PATH = r"e:\wallpaper_redis"
 
-class Wallpaper():
+
+class Wallpaper:
 
     def __init__(self, dir_path=None):
-        self.dir_path = dir_path or r"e:\wallpaper_redis"       # 文件保存路径
-        self.urlfmt = "http://sj.zol.com.cn{}"
-        for url in self.geturls():
-            rd.sadd('urls', url)
+        self.dir_path = dir_path or SAVE_PATH  # 文件保存路径
+        self.crawl_url = "http://sj.zol.com.cn{}"
+        for url in self.get_urls():
+            rd.sadd("urls", url)
 
     def mkdir(self, folder_name):
-        """ 新建文件夹并切换到该目录下 """
-        path = os.path.join(self.dir_path, folder_name)
+        """
+        新建文件夹并切换到该目录下
+
+        :param folder_name: 文件夹名称
+        """
+        path = os.path.join(SAVE_PATH, folder_name)
         # 如果目录已经存在就不用再次爬取了，去重，提高效率。存在返回 False，否则反之
         if not os.path.exists(path):
             os.makedirs(path)
@@ -29,70 +41,96 @@ class Wallpaper():
         return False
 
     def save(self, src, name):
-        """ 保存图片到本地 """
+        """
+        保存图片到本地
+
+        :param src: 图片 src
+        :param name: 保存图片名
+        """
         try:
-            img = requests.get(src, headers=headers)
-            with open(name + ".jpg", 'ab') as f:
+            img = requests.get(src, headers=HEADERS)
+            with open(name + ".jpg", "ab") as f:
                 f.write(img.content)
-                print("Success!")
+                print("{}.jpg save Successfully".format(name))
         except:
-            print("Failed!")
+            pass
 
-    def clear(self, dir):
-        """ 删除空文件夹 """
-        if os.path.exists(dir):
-            if os.path.isdir(dir):
-                for d in os.listdir(dir):
-                    path = os.path.join(dir, d)
+    def clear(self, dir_path):
+        """
+        删除空文件夹
+
+        :param dir_path: 文件夹路径
+        """
+        if os.path.exists(dir_path):
+            if os.path.isdir(dir_path):
+                for d in os.listdir(dir_path):
+                    path = os.path.join(dir_path, d)
                     if os.path.isdir(path):
-                        self.clear(path)     # 递归删除空文件夹
-            if not os.listdir(dir):
-                os.rmdir(dir)
-                print("remove the empty dir: {}".format(dir))
+                        # 递归删除空文件夹
+                        self.clear(path)
+            if not os.listdir(dir_path):
+                os.rmdir(dir_path)
+                print("remove the empty dir: {}".format(dir_path))
 
-    def geturls(self):
-        """ 获取壁纸套图地址 """
+    def get_urls(self):
+        """
+        获取壁纸套图地址
+
         # 1080x1920(iphone6s plus)
         # 800x1280
         # 768x1280
         # 750x1334(iphone6)
         # 720x1280
         # 640x1336(iphone5s)
-        # 主爬取地址，可自行配置
+        """
         url = "http://sj.zol.com.cn/bizhi/1080x1920/{}.html"
         _urls = set()
         for url in [url.format(page) for page in range(1, 51)]:
-            r = requests.get(url, headers=headers).text
-            for u in [self.urlfmt.format(u) for u in re.findall('<a class="pic" href="(.*?)"', r)]:
+            req = requests.get(url, headers=HEADERS).text
+            for u in [
+                self.crawl_url.format(u)
+                for u in re.findall('<a class="pic" href="(.*?)"', req)
+            ]:
                 _urls.add(u)
         return _urls
 
     def run(self):
         """ 启动爬虫 """
-        url = rd.spop('urls')
+        url = rd.spop("urls")
         while url:
             try:
-                result = requests.get(url, headers=headers).text
+                html = requests.get(url, headers=HEADERS).text
                 # 壁纸套图名，也作文件夹名
-                title = re.findall('<h1><a href=.*?>(.*?)</a.*?</h1>', result)[0]
+                title = re.findall("<h1><a href=.*?>(.*?)</a.*?</h1>", html)[0]
                 # 壁纸套图张数
-                max_cnt = re.findall('</a><span>（(.*?)）</span>', result)[0].split("/")[1]
+                max_cnt_pattern = "</a><span>（(.*?)）</span>"
+                max_cnt = re.findall(max_cnt_pattern, html)[0].split("/")[1]
                 if self.mkdir(title):
                     # 按点击下一页循环下载
                     for _ in range(int(max_cnt)):
-                        next_page = re.findall('class ="next" href="(.*?)"', result)[0]
-                        img_url = re.findall('id="750x1334" href="(.*?)">750x1334</a>', result)
+                        next_page = re.findall(
+                            'class ="next" href="(.*?)"', html
+                        )
+                        img_url = re.findall(
+                            'id="750x1334" href="(.*?)">750x1334</a>', html
+                        )
                         # 像素未达到 750x1334 的不进行下载
                         img = img_url[0] if img_url else []
                         if img:
-                            r = requests.get(self.urlfmt.format(img), headers=headers).text
-                            img_src = re.findall('<img src="(.*?)"', r)[0]
+                            req = requests.get(
+                                self.crawl_url.format(img), headers=HEADERS
+                            ).text
+                            img_src = re.findall('<img src="(.*?)"', req)[0]
                             self.save(img_src, img[-12:-5])
-                        result = requests.get(self.urlfmt.format(next_page), headers=headers).text
-                url = rd.spop('urls')       # 再次弹出 url，循环爬取
+                        html = requests.get(
+                            self.crawl_url.format(next_page[0]),
+                            headers=HEADERS,
+                        ).text
+                url = rd.spop("urls")  # 再次弹出 url，循环爬取
             except Exception as e:
                 print(e)
-        self.clear(self.dir_path)       # 最后清除空文件夹
+        self.clear(self.dir_path)  # 最后清除空文件夹
+
 
 if __name__ == "__main__":
     Wallpaper().run()
